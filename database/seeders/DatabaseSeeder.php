c7327bfa9968565b4e1738d8546acb069dd38a81
snowfallen <bondarenkoov1998@gmail.com>
2023-06-15 20:47:08 +0200

    create seeders for db

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,9 +3,7 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
-use App\Models\User;
 use Illuminate\Database\Seeder;
-use Spatie\Permission\Models\Role;
 
 class DatabaseSeeder extends Seeder
 {
@@ -16,16 +14,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        Role::create(['name' => 'admin']);
-        Role::create(['name' => 'writer']);
-        Role::create(['name' => 'user']);
-        // \App\Models\User::factory(10)->create();
-        $adminRole = Role::where('name', 'admin')->first();
-        $admin = User::factory()->create([
-            'name' => 'admin',
-            'email' => 'admin@example.com',
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi'
-        ]);
-        $admin->assignRole($adminRole);
+        $this->call(RoleSeeder::class);
+        $this->call(UserSeeder::class);
     }
 }
---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(): void
+    {
+        $adminRole = Role::where('name', 'admin')->first();
+        $admin = User::create([
+            'name' => 'admin',
+            'email' => 'admin@example.com',
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi'
+        ]);
+        $admin->assignRole($adminRole);
+    }
+}
