4a77d69e2cff1bd5ad52027ba01ead839fe84bff
snowfallen <bondarenkoov1998@gmail.com>
2023-06-17 12:12:39 +0200

    add queue

---
+++ app/Jobs/SendNewPostNotification.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Mail\NewPostNotification;
+use App\Models\User;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Mail;
+
+class SendNewPostNotification implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        $users = User::all();
+
+        foreach ($users as $user) {
+            Mail::to($user->email)->send(new NewPostNotification);
+        }
+    }
+}
